0ac867c6fc8e854e2dc1134e032611b73a427ed5
Sarah Chi <sarah_chi@Sarahs-MacBook-Pro.local>
2020-10-02 16:56:36 -0700

    format

--- src/components/Layout/LoginLink.js
+++ src/components/Layout/LoginLink.js
@@ -1,21 +1,23 @@
 import React from 'react';
-import {Link} from 'react-router-dom'
+import {
+	Link
+} from 'react-router-dom'
 import '../../css/styles.css';
 import Signin from '../Pages/Signin'
 
 
 const LoginLinks = () => {
 
-	return(
-		<div>
-		<a class="nav-link" href="/Signin">
-            <button type="button" class="btn btn-info btn-primary  btn-sm my-0 ">Login</button>
-        </a>         
-
-        </div>
-
-
-
+	return ( <
+		div >
+		<
+		a class = "nav-link"
+		href = "/Signin" >
+		<
+		button type = "button"
+		class = "btn btn-info btn-primary  btn-sm my-0 " > Login < /button> <
+		/a>          <
+		/div>
 	)
 }
 
