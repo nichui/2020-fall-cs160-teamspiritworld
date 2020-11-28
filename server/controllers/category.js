9eef2b64e6f04804d38c414f736a8e5f9180f25f
Kanchanak <43360935+Kanchanakkh@users.noreply.github.com>
2020-11-27 19:52:27 -0500

    create a new folder controller
    moved functionality of user's (aka auth) api routes as well as category for better organization

---
+++ server/controllers/category.js
@@ -0,0 +1,46 @@
+const Category = require('../models/category')
+const slugify = require('slugify')
+
+exports.create = async(req, res) => {
+    try{
+        const {name} = req.body
+        //const category = await new Category({name, slug: slugify(name)}).save()
+        res.json(await new Category({name, slug: slugify(name)}).save())
+    }catch (err){
+        console.log(err)
+        res.status(400).send('Create category failed')
+    }
+    
+}
+exports.list = async(req, res) => {
+    const categories = await Category.find({}).sort({createdAt: -1}).exec()
+    res.json(categories)
+    console.log('CATEGORY LIST =====> ', categories)
+    //
+}
+exports.read = async(req, res) => {
+    let category = await Category.findOne({slug: req.params.slug}).exec()
+    res.json(category)
+}
+exports.update = async(req, res) => {
+    const {name} = req.body;
+    try{
+        const updated = await Category.findOneAndUpdate({slug: req.params.slug}, {name, slug: slugify(name) },
+        {new: true})
+        res.json(updated)
+
+    }catch(err){
+        res.status(400).send("Create update failed")
+    }
+    
+}
+exports.remove = async(req, res) => {
+
+    try{
+        const deleted = await Category.findOneAndDelete({slug: req.params.slug})
+        res.json(deleted)
+
+    }catch(err){
+        res.status(400).send("Category delete failed")
+    }
+}
