78e2a7dcfaa75a0b2692fcb2604ad492770d0511
Danh Nguyen <vn1007347@gmail.com>
2020-11-27 23:07:17 -0800

    Updated Controller for reviews, users, and structured routes clearer

--- ResourceRoutes/reviews.js
+++ ResourceRoutes/reviews.js
@@ -5,27 +5,14 @@ const catchAsync = require('../WebpageDisplay/Utility/CatchAsync');
 const {resourceSchema, reviewSchema} = require('../ValidateSchemas.js');
 const Resource = require('../DataDisplay/resource');
 const Review = require('../DataDisplay/reviewResource');
+const reviews = require('../controllers/reviews')
 const ValidateError = require('../WebpageDisplay/Utility/ValidateError');
 
 
 
-router.post('/', isLoggedIn ,validateReview, catchAsync(async(request, response) =>{
-    const resource = await Resource.findById(request.params.id);
-    const review = new Review(request.body.review);
-    review.author = request.user._id;
-    resource.reviews.push(review);
-    await review.save();
-    await resource.save();
-    request.flash('success', 'New Review is successfully added!!!');
-    response.redirect(`/resources/${resource._id}`);
-}))
 
-router.delete('/:reviewId', isLoggedIn, isReviewOwner, catchAsync(async(request, response) =>{
-    const{id, reviewId} = request.params;
-    await Resource.findByIdAndUpdate(id, {$pull: { reviews: reviewId}});
-    await Review.findByIdAndDelete(request.params.reviewId);
-    request.flash('success', 'Review is successfully deleted!!!')
-    response.redirect(`/resources/${id}`);
-}))
+router.post('/', isLoggedIn ,validateReview, catchAsync(reviews.createReview))
+
+router.delete('/:reviewId', isLoggedIn, isReviewOwner, catchAsync(reviews.deleteReview))
 
 module.exports = router;
