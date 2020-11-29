c7f1c08a78289e0ec015d36dedb01a778c6a6510
Danh Nguyen <vn1007347@gmail.com>
2020-11-29 03:01:34 -0800

    Added Marker for the map, centered marker and made marker point at the right location as we typed places in

--- public/Scripts/showPageMap.js
+++ public/Scripts/showPageMap.js
@@ -2,6 +2,10 @@ mapboxgl.accessToken = mapToken;
 const map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 4 // starting zoom
+    center: resource.geometry.coordinates, // starting position [lng, lat]
+    zoom: 10 // starting zoom
 });
+
+new mapboxgl.Marker()
+.setLngLat(resource.geometry.coordinates)
+.addTo(map)
