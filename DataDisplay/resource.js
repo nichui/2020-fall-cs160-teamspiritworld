a4e367d3d60d7d3a57fa5bdbbfcecedca0db0ee4
Danh Nguyen <vn1007347@gmail.com>
2020-11-29 01:12:48 -0800

    Added thumbnail for Images for them to look nicer and easier to keep track

--- DataDisplay/resource.js
+++ DataDisplay/resource.js
@@ -2,14 +2,18 @@ const mongoose = require('mongoose');
 const Review = require('./reviewResource');
 const Schema = mongoose.Schema;
 
+const ImageSchema = new Schema({
+    url: String,
+    filename: String
+})
+
+ImageSchema.virtual('thumbnail').get(function(){
+    return this.url.replace('/upload', '/upload/w_300');
+});
+
 const ResourceSchema = new Schema({
     title: String,
-    images: [
-        {
-            url: String,
-            filename: String
-        }
-    ],
+    images: [ImageSchema],
     content: String, //price
     category: String, // description
     location: String,
