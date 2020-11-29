252f8de52d81e16bda405f3ae921f1d020823c31
Danh Nguyen <vn1007347@gmail.com>
2020-11-29 05:58:10 -0800

    Added correspoding points to SJSU and found Duncan and other building. Will change zoom lv later.

--- public/Scripts/resourceMap.js
+++ public/Scripts/resourceMap.js
@@ -14,8 +14,7 @@ map.on('load', function () {
         type: 'geojson',
 // Point to GeoJSON data. This example visualizes all M1.0+ earthquakes
 // from 12/22/15 to 1/21/16 as logged by USGS' Earthquake hazards program.
-        data:
-            'https://docs.mapbox.com/mapbox-gl-js/assets/earthquakes.geojson',
+        data:resources,
         cluster: true,
         clusterMaxZoom: 14, // Max zoom to cluster points on
         clusterRadius: 50 // Radius of each cluster when clustering points (defaults to 50)
