a45e7992265042b93d7321e86c340366e8070054
Danh Nguyen <vn1007347@gmail.com>
2020-12-01 22:54:19 -0800

    Added test cases for login page

---
+++ cypress/integration/TestSpiritWorld/Locators.spec.js
@@ -0,0 +1,6 @@
+/// <reference types="Cypress" />
+describe('Locating Elements', function(){
+    it('Verify types of locators', function(){
+        cy.visit("http://localhost:3000") // Opens the URL
+    })
+})
